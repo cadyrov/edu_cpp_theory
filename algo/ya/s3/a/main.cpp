295e26465d9041325153fb8715fbbd7eb8dc33a6
Ivan Kadyrov <i.kadyrov@mercuryo.io>
2026-04-07 10:08:20 +0300

    init

---
+++ algo/ya/s3/a/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+
+
+
+void brac(int open, int close, int n, std::string in) {
+    if (open == n && close == n) {
+        std::cout << in << std::endl;
+    }
+
+    if (open < n) {
+        in.push_back('(');
+        brac(open+1, close, n, in);
+        in.pop_back();
+    }
+
+     if (close < open) {
+        in.push_back(')');
+        brac(open, close+1, n, in);
+        in.pop_back();
+    }
+  
+}
+
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    brac(0, 0, n, "");
+
+    return 1;
+}
