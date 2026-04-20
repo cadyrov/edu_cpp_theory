abf565ab58e8382f627d82ca236c60c2a262bafd
Ivan Kadyrov <i.kadyrov@mercuryo.io>
2026-04-20 22:51:13 +0300

    init

--- algo/ya/s3/o/main.cpp
+++ algo/ya/s3/o/main.cpp
@@ -3,12 +3,42 @@
 #include <algorithm>
 #include <cassert>
 
+long long binDiff(const std::vector<long long>& in, int k) {
+    auto diff = [](const std::vector<long long>& in, long long df ){
+        long long result = 0;
+        for (int i = 0; i < in.size() - 1; ++i) {
+            // Проверяем переполнение перед сложением
+            long long target = in[i] + df;
+            auto it = std::upper_bound(in.begin() + i + 1, in.end(), target);
+            result += std::distance(in.begin() + i + 1, it);
+        }
+        return result;
+    };
+
+    long long min_diff = 0;
+    long long max_diff = in[in.size() - 1] - in[0];
+
+    while (min_diff < max_diff) {
+        long long mid_diff = min_diff + (max_diff - min_diff) / 2;
+        long long count = diff(in, mid_diff);
+
+        if (count >= k) {
+            max_diff = mid_diff;
+        } else {
+            min_diff = mid_diff + 1;
+        }
+    }
+
+    return min_diff;
+}
+
+
 int main() {
     int n;
     int k;
     std::cin >> n;
 
-    std::vector<int> plates{};
+    std::vector<long long> plates{};
 
     plates.resize(n);
 
@@ -20,46 +50,9 @@ int main() {
 
     std::sort(plates.begin(), plates.end());
 
-    // в отсортированном массиве минимальная разница будет у соседних первых элементов
-    // масимвальная у первого и последнего
-
-    // для каждого целого числа между ними мы по массиву ищем количеству разниц бинарным поиском
-    // это условно квадратичный алгоритм но ограничение в остортированом массиве будет дистанция
-
-    // опредлим функцию нахождения количества чисел меньше или равной разнице для числа n
-
-    auto diff = [](const std::vector<int>& in, int df ){
-        int result = 0;
-        for (int i = 0; i < in.size() - 1; ++i) {
-            for (int j = i + 1; j < in.size(); ++j) {
-                if (in[j] - in[i] <= df) {
-                    ++result;
-                }
-            }
-        }
-        return result;
-    };
-
-    assert(diff(std::vector<int>{1,2,3,4}, 2) == 5);
-    assert(diff(std::vector<int>{1,1,3,4}, 1) == 2);
-
-
-    int min_diff = 0;
-    int max_diff = plates[plates.size() - 1] - plates[0];
-
-    while (min_diff < max_diff) {
-        int mid_diff = min_diff + (max_diff - min_diff) / 2;
-        int count = diff(plates, mid_diff);
-
-        if (count >= k) {
-            max_diff = mid_diff;
-        } else {
-            min_diff = mid_diff + 1;
-        }
-    }
-
-    std::cout << min_diff << std::endl;
+    std::cout << binDiff(plates, k) << std::endl;
     
 
     return 0;
 }
+
