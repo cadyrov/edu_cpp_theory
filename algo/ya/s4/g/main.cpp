902e4fe8c91a4c2b33c75ab7411791c4c109bfd8
Ivan Kadyrov <i.kadyrov@mercuryo.io>
2026-05-12 08:03:20 +0300

    init

--- algo/ya/s4/g/main.cpp
+++ algo/ya/s4/g/main.cpp
@@ -1,7 +1,59 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include <set>
+#include <algorithm>
 
 int main() {
+    int n;
+    long long sum;
+
+    std::cin >> n >> sum;
+
+    std::vector<long long> numbers(n);
+
+    if (n ==0) {
+        std::cout << 0 << '\n';
+        return 0;
+    }
+
+    for (int i = 0; i < n; ++i) {
+        std::cin >> numbers[i];
+    }
+
+    std::unordered_map<long long, std::vector<std::vector<int>>> numbers_map;
+
+    for (int i = 0; i < numbers.size()-1; ++i) {
+        for (int j = i+1; j < numbers.size(); ++j) {
+            numbers_map[numbers[i] + numbers[j]].push_back(std::vector<int>{i, j});
+        }
+    }
+
+    std::set<std::vector<long long>> result; 
+
+
+    for (int i = 0; i < numbers.size()-1; ++i) {
+        for (int j = i+1; j < numbers.size(); ++j) {
+            auto vc = numbers_map.find(sum - numbers[i] - numbers[j]);
+            if (vc != numbers_map.end()) {
+                for (int p =0; p < (*vc).second.size(); ++p) {
+                    int idx1 = (*vc).second[p][0];
+                    int idx2 = (*vc).second[p][1];
+                    if (idx1 != i && idx1 != j && idx2 != i && idx2 != j) {
+                        std::vector<long long> quad = {numbers[idx1], numbers[idx2], numbers[i], numbers[j]};
+                        std::sort(quad.begin(), quad.end());
+                        result.insert(quad);
+                    }
+                }
+            }
+        }
+    }
+
+    std::cout << result.size() << '\n';
+    for (auto quad :result) {
+        std::cout << quad[0] << " " << quad[1] << " " 
+            << quad[2] << " " << quad[3] << '\n';
+    }
 
 }
--- algo/ya/s4/i/main.cpp
+++ algo/ya/s4/i/main.cpp
@@ -1,7 +1,59 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
+#include <algorithm>
+
+
+
+int hash(std::string& s) {
+    std::sort(s.begin(), s.end());
+    
+    int h = 0;
+
+    int base = 41;
+    int mod = (1 << 30) - 1;
+
+    long long current_base = 1;
+
+    for (char c : s) {
+        h = (h * current_base + c) % mod;
+        current_base = (current_base * base) % mod;
+    }
+
+    return h;
+}
 
 int main() {
+    int n;
+    std::string in;
 
+    std::map<int, std::vector<int>> word_count;
+
+    std::cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        std::cin >> in;
+        
+        word_count[hash(in)].push_back(i);
+    }
+
+
+    std::vector<std::vector<int>> groups;
+    groups.reserve(word_count.size());
+    
+    for (const auto& p : word_count) {
+        groups.emplace_back(std::move(p.second));
+    }
+
+    std::sort(groups.begin(), groups.end());
+
+    
+    for (const auto& group : groups) {
+        for (int i : group) {
+            std::cout << i << " ";
+        }
+
+        std::cout << std::endl;
+    }
 }
