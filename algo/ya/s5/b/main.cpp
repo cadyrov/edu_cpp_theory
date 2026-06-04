7ec2281de4901680dd5de60877e97115cecad163
Ivan Kadyrov <i.kadyrov@mercuryo.io>
2026-06-04 13:26:10 +0300

    feat(MRC-8696):-add-rule-type

---
+++ algo/ya/s5/b/main.cpp
@@ -0,0 +1,54 @@
+#ifndef REMOTE_JUDGE
+struct Node {  
+  int value;  
+  const Node* left = nullptr;  
+  const Node* right = nullptr;
+  Node(int value, Node* left, Node* right) : value(value), left(left), right(right) {}
+};  
+#endif
+
+#ifdef REMOTE_JUDGE
+#include "solution_tree.h"
+#endif
+#include <cmath>
+#include <iostream>
+#include <cassert>
+#include <algorithm>
+
+using namespace std;
+
+
+int height(const Node* root) {
+    if (!root) {
+        return 0;
+    }
+
+    int l = height(root->left);
+    int r = height(root->right);
+
+    if (l == -1 || r == -1 || std::abs(l - r) > 1) {
+        return -1;
+    }
+
+    return std::max(l, r) + 1;
+}
+
+bool Solution(const Node* root) {
+    return height(root) != -1;
+} 
+
+
+#ifndef REMOTE_JUDGE
+void test() {
+    Node node1({1, nullptr, nullptr});
+    Node node2({-5, nullptr, nullptr});
+    Node node3({3, &node1, &node2});
+    Node node4({10, nullptr, nullptr});
+    Node node5({2, &node3, &node4});
+    assert(Solution(&node5));
+}
+
+int main() {
+  test();
+}
+#endif
