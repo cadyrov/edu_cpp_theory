a9a332016a4837691830156ea799b1e4a451583b
Ivan Kadyrov <i.kadyrov@mercuryo.io>
2026-08-12 12:51:15 +0300

    init

---
+++ ya_cpp/4_stdlib/task/17/build/CMakeFiles/associative_containers_benchmark.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/associative_containers_benchmark.dir/main.cpp.o"
+  "CMakeFiles/associative_containers_benchmark.dir/main.cpp.o.d"
+  "associative_containers_benchmark"
+  "associative_containers_benchmark.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/associative_containers_benchmark.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
